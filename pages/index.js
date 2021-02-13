970cb64c8b65cab8a72d6401679e0a47c3b44258
Alisson Dias <alissonsalinos@outlook.com>
2021-02-13 16:09:07 -0300

    instalura-base 2 aula

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import styled from 'styled-components'
+import React from 'react';
+import Menu from '../src/components/commons/Menu';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
-
-export default function Home() {
-  return <Title>My page</Title>
+export default function Home() {  
+  return (
+  <div>
+    <Menu />
+  </div>  
+  )
 }
